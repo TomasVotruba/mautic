65ac6fc1b8926c247ca4dab95c33a21c4842c952
fedys <miroslav.fedeles@gmail.com>
2026-06-18 10:50:39 +0200

    WIP

--- .devcontainer/setup-project.sh
+++ .devcontainer/setup-project.sh
@@ -12,17 +12,8 @@ wait_for_docker() {
 
 wait_for_docker
 
-# This file is called in three scenarios:
-# 1. fresh creation of devcontainer
-# 2. rebuild
-# 3. full rebuild
-
-ddev debug download-images
-
 ddev poweroff
-
 ddev -v
-
 ddev config global --web-environment="MAUTIC_URL=https://${CODESPACE_NAME}-80.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN},PHPMYADMIN_URL=https://${CODESPACE_NAME}-8036.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN},MAILHOG_URL=https://${CODESPACE_NAME}-8025.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 
 cat <<EOF >.ddev/docker-compose.phpmyadmin_norouter.yaml
