7f6c99aa33850120ff388c83cd39701a0fb5794d
fedys <miroslav.fedeles@gmail.com>
2026-06-19 11:48:55 +0200

    WIP

--- .devcontainer/prebuild-project.sh
+++ .devcontainer/prebuild-project.sh
@@ -3,4 +3,6 @@ set -e
 
 curl -fsSL https://ddev.com/install.sh | bash
 ddev start -y --skip-hooks
+ddev composer install --no-interaction
+ddev exec rm -f /var/tmp/logpipe # fixes randomly occurring "mkfifo: cannot create fifo '/var/tmp/logpipe': File exists"
 ddev poweroff
