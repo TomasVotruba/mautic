44eb58a5f259403799686c8d950f0aa96f2fe338
Tomas Votruba <tomas.vot@gmail.com>
2026-06-20 15:54:22 +0200

    apply on repository classes

--- rector.php
+++ rector.php
@@ -6,11 +6,7 @@ use MauticRector\UnserializeToSerializerDecodeRector;
 use Rector\CodeQuality\Rector\ClassMethod\OptionalParametersAfterRequiredRector;
 use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
 use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
-use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
-use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
-use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\TypeDeclaration\Rector\Class_\ReturnTypeFromStrictTernaryRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
@@ -61,7 +57,7 @@ return RectorConfig::configure()
             // require bit test update
             __DIR__.'/app/bundles/LeadBundle/Model/LeadModel.php',
             // array vs doctrine collection
-            __DIR__.'/app/bundles/CoreBundle/Entity/TranslationEntityTrait.php',
+            __DIR__ . '/app/bundles/CoreBundle/Entity/TranslationEntityTrait.php',
         ],
 
         // Avoiding breaking BC breaks with forced return types in public methods
@@ -73,43 +69,6 @@ return RectorConfig::configure()
         // lets handle later, once we have more type declaratoins
         RecastingRemovalRector::class,
 
-        // Rector 2.4.5 false positive: setFromForSingleMessage() and setReplyToForSingleMessage()
-        // are both called from send(), but dead code analysis incorrectly treats them as unused
-//        RemoveUnusedPrivateMethodRector::class => [
-//            __DIR__.'/app/bundles/EmailBundle/Helper/MailHelper.php',
-//        ],
-
-        RemoveUnusedPrivatePropertyRector::class => [
-            // entities
-            __DIR__.'/app/bundles/UserBundle/Entity',
-        ],
-
-        TypedPropertyFromStrictConstructorRector::class => [
-            // entities magic
-            __DIR__.'/app/bundles/LeadBundle/Entity',
-        ],
-
-//        ClassPropertyAssignToConstructorPromotionRector::class => [
-//            __DIR__.'/app/bundles/CacheBundle/EventListener/CacheClearSubscriber.php',
-//            __DIR__.'/app/bundles/ReportBundle/Event/ReportBuilderEvent.php',
-//            // false positive
-//            __DIR__.'/app/bundles/CoreBundle/DependencyInjection/Builder/BundleMetadata.php',
-//        ],
-
-        TypedPropertyFromAssignsRector::class => [
-            '*/Entity/*',
-        ],
-
-       // // Skip the rule file itself
-//        UnserializeToSerializerDecodeRector::class => [
-//            __DIR__.'/rector',
-//        ],
-
         // handle later with full PHP 8.0 upgrade
         OptionalParametersAfterRequiredRector::class,
-
-        // handle later, case by case as lot of chnaged code
-        //RemoveAlwaysTrueIfConditionRector::class => [
-        //    __DIR__.'/app/bundles/FormBundle/Controller/FormController.php',
-        // ],
     ]);
