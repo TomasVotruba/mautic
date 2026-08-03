dc0ab35c1d98c3c50f71690bc895ce3d88e689cd
Tomas Votruba <tomas.vot@gmail.com>
2026-08-03 22:31:28 +0200

    [twig] flip functison to as twig function attribute

--- app/bundles/CoreBundle/Twig/Extension/VersionExtension.php
+++ app/bundles/CoreBundle/Twig/Extension/VersionExtension.php
@@ -5,23 +5,15 @@ declare(strict_types=1);
 namespace Mautic\CoreBundle\Twig\Extension;
 
 use Mautic\CoreBundle\Helper\AppVersion;
-use Twig\Extension\AbstractExtension;
-use Twig\TwigFunction;
 
-final class VersionExtension extends AbstractExtension
+final class VersionExtension
 {
     public function __construct(
         private readonly AppVersion $appVersion,
     ) {
     }
 
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction('mauticAppVersion', $this->getVersion(...)),
-        ];
-    }
-
+    #[\Twig\Attribute\AsTwigFunction(name: 'mauticAppVersion')]
     public function getVersion(): string
     {
         return $this->appVersion->getVersion();
