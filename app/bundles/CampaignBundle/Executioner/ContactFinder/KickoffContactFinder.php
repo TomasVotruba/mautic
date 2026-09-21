b06e6ed4c3acab873fee227d3ed7634180d3c7e5
Tomas Votruba <tomas.vot@gmail.com>
2026-09-21 22:20:00 +0200

    add sets from symplify

--- app/bundles/CampaignBundle/Executioner/ContactFinder/KickoffContactFinder.php
+++ app/bundles/CampaignBundle/Executioner/ContactFinder/KickoffContactFinder.php
@@ -28,7 +28,7 @@ readonly class KickoffContactFinder
     public function getContacts(?int $campaignId, ContactLimiter $limiter): ArrayCollection
     {
         // Get list of all campaign leads; start is always zero in practice because of $pendingOnly
-        $campaignContacts = $this->campaignRepository->getPendingContactIds($campaignId, $limiter);
+        $campaignContacts = $this->campaignRepository->getPendingContactIds((int) $campaignId, $limiter);
 
         if ([] === $campaignContacts) {
             // No new contacts found in the campaign
@@ -53,7 +53,7 @@ readonly class KickoffContactFinder
 
     public function getContactCount(?int $campaignId, array $eventIds, ContactLimiter $limiter): int
     {
-        $countResult = $this->campaignRepository->getCountsForPendingContacts($campaignId, $eventIds, $limiter);
+        $countResult = $this->campaignRepository->getCountsForPendingContacts((int) $campaignId, $eventIds, $limiter);
 
         return $countResult->getCount();
     }
